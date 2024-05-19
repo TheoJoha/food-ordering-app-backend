3c6c00a3079fcd628524c03dc0bd6fd0af225292
Theodor Johansson <t.j.u@telia.com>
2024-05-19 18:05:29 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, {Request, Response} from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute"
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("connected to db"))
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+app.get("/health", async (req: Request, res: Response) => {
+    res.send({message: "health ok!"})
+})
+
+app.use("/api/my/user", myUserRoute);
+
+app.listen(3003, () => {
+    console.log("server started on localhost:3003")
+})
